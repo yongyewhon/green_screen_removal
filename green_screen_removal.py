4cd587b4327b54b5ad6780bd6152210cd2cbc94e
yongyewhon <151745867+yongyewhon@users.noreply.github.com>
2024-03-15 18:10:28 +0800

    Add files via upload

--- green_screen_removal.py
+++ green_screen_removal.py
@@ -5,16 +5,17 @@ import os
 directory_path = "./Images/" #input image
 #background_image = cv2.imread("./Background/Eiffel-Tower.jpg")
 #background_image = cv2.imread("./Background/Nature-Scenery.jpg")
-background_image = cv2.imread("./Background/Sky.jpg")
+#background_image = cv2.imread("./Background/Sky.jpg")
+#background_image = cv2.imread("./Background/Sky2.jpg")
 #background_image = cv2.imread("./Background/Spotlight-Stage.jpg")
-#background_image = cv2.imread("./Background/Stage.jpg")
+background_image = cv2.imread("./Background/Stage.jpg")
 bg_height, bg_width, _ = background_image.shape
 
 for path_img in os.listdir(directory_path):
     if path_img.endswith(".jpg"):
         input_image = cv2.imread(os.path.join(directory_path, path_img))
         image = cv2.resize(input_image, (bg_width, bg_height))
-
+        print(bg_height, bg_width)
         img = image.copy()
         lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
         a_channel = lab[:,:,1]
@@ -27,8 +28,10 @@ for path_img in os.listdir(directory_path):
         #cv2.waitKey(0)
         
         final = np.where(masked==0, background_image, masked)
-        cv2.imshow("final", final)
         cv2.imwrite("./Result/" + f"Save_{path_img}", final)
         print(f"Save_{path_img}")
+        cv2.putText(final, "Press any key to continue...", (int(bg_width/100), int(bg_height/20)), 
+                    cv2.FONT_HERSHEY_COMPLEX, bg_height*0.002, (255, 255, 255), int(bg_height*0.004))
+        cv2.imshow("final", final)
         cv2.waitKey(0)
 cv2.destroyAllWindows()
